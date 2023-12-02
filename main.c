8622a2fbd164d44c96866b63190f969bbbcafe79
SuraSurow <126407492+SuraSurow@users.noreply.github.com>
2023-12-02 11:03:19 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,121 @@
+
+
+#include <iostream>
+
+uint8_t swapNoLoop(uint8_t a);
+uint8_t swapLoop(uint8_t a);
+
+int main()
+{
+    uint8_t a = 0x36;
+    uint8_t wynik = swapLoop(a);
+
+    std::cout << std::hex << (int)wynik << '\n';
+
+    return 0;
+}
+
+
+uint8_t swapLoop(uint8_t a)
+{
+    uint8_t ret = 0;
+
+
+    _asm
+    {
+        xor eax, eax
+        xor ebx, ebx
+
+
+
+        mov bl, 0x01
+        mov bh, 0x80
+       
+
+        STARTLOOP :
+        mov al, a
+        and al, bh
+
+            cmp al, bh
+            jnz NIEJEST
+            or ah, bl
+            NIEJEST :
+            shl bl, 1
+            shr bh, 1
+            cmp bh, 0x00
+            jz ENDLOOP
+            jmp STARTLOOP
+            ENDLOOP :
+        mov ret ,ah
+    }
+    return ret;
+}
+
+
+
+
+uint8_t swapNoLoop(uint8_t a)
+{
+    uint8_t ret = 0;
+
+    _asm
+    {
+        xor eax, eax
+        xor ebx, ebx
+
+        mov al, a
+        mov bl, 0x01
+        and al, bl
+        shl al, 7
+        or ah, al
+
+        mov al, a
+        mov bl, 0x02
+        and al, bl
+        shl al, 5
+        or ah, al
+
+        mov al, a
+        mov bl, 0x04
+        and al, bl
+        shl al, 3
+        or ah, al
+
+        mov al, a
+        mov bl, 0x08
+        and al, bl
+        shl al, 1
+        or ah, al
+
+        mov al, a
+        mov bl, 0x10
+        and al, bl
+        shr al, 1
+        or ah, al
+
+        mov al, a
+        mov bl, 0x20
+        and al, bl
+        shr al, 3
+        or ah, al
+
+        mov al, a
+        mov bl, 0x40
+        and al, bl
+        shr al, 5
+        or ah, al
+
+        mov al, a
+        mov bl, 0x80
+        and al, bl
+        shr al, 7
+        or ah, al
+
+        mov ret, ah
+
+
+
+
+    }
+    return ret;
+}
